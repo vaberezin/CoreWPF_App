415048d9cf32d05c233e9a37ea5c65e334617e1d
vaberezin <vaberezin@protonmail.com>
2020-07-22 23:04:22 +0300

    Written A()+psy_mid(). Write z_-methods

--- RainFlowCalcWindow.xaml.cs
+++ RainFlowCalcWindow.xaml.cs
@@ -13,10 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using CoreWpfApp.RainCalcs; //����������111
-
-
-
-
+using MySqlX.XDevAPI.Common;
 
 namespace CoreWpfApp
 {
@@ -30,26 +27,21 @@ namespace CoreWpfApp
         {
             
             InitializeComponent();
+
             Region.ItemsSource = Regions;
             Place.ItemsSource = Places;
 
-            //binding.ElementName = "F_tot";
-            //binding.Path = new PropertyPath("Text");
-            //F_tot.SetBinding()
-
             RainVariables rainVariables = new RainVariables();
-                
-            
-            
+              
         }
         List<string> Regions = ListForCombobox.getRegionList();
         List<string> Places = ListForCombobox.getPlaceList();
 
         
-        
-
         void SetValuesFromComboboxes(RainVariables rainVariables)
         {
+            
+
             rainVariables.F = Double.Parse(F_tot.Text);
             rainVariables.F_road = Double.Parse(F_road.Text);
             rainVariables.F_roof = Double.Parse(F_roof.Text);
@@ -64,12 +56,12 @@ namespace CoreWpfApp
             rainVariables.v_p = Double.Parse(CollectorVelocity.Text);
             rainVariables.N_sections = Int32.Parse(SectionsNumber.Text);
             rainVariables.n = 
-            rainVariables.A();
-            rainVariables.Psy_mid();
-            rainVariables.Z_mid();
-            rainVariables.t_r();
-            rainVariables.t_can();
-            rainVariables.t_p();
+            getA(rainVariables); //+
+            getPsy_mid(rainVariables);+
+            getZ_mid();
+            gett_r();
+            gett_can();
+            gett_p();
 
             //Target VALUES:->
             rainVariables.Q_rRain();
@@ -104,13 +96,59 @@ namespace CoreWpfApp
         }
 
         double getPsy_mid(RainVariables rainvariables){
-            double _Psy_mid = 0; 
+            double _Psy_mid = 0;
 
-            _Psy_mid = 
-              
+            double K_gravel = rainvariables.F_gravel * 0.4; //tabl10 rek
+            double K_road = rainvariables.F_road * 0.95; //tabl10 rek
+            double K_roof = rainvariables.F_roof * 0.95; //tabl10 rek
+            double K_green = rainvariables.F_green * 0.1; //tabl10 rek
+
+
+            _Psy_mid = (K_gravel + K_green + K_road + K_roof) / rainvariables.F;
+
             return _Psy_mid;
         }
         
+        double getZ_mid(RainVariables rainvariables)
+        {
+            double _Z_mid = 0;
 
+            double K_gravel = rainvariables.F_gravel * 0.125; //tabl10 rek
+            double K_road = rainvariables.F_road * //; //tabl10 rek
+            double K_roof = rainvariables.F_roof * //; //tabl10 rek
+            double K_green = rainvariables.F_green * 0.038; //tabl10 rek
+
+
+
+            return _Z_mid;
+        }
+
+        double z_roadAndRoof(RainVariables rainVariables)
+        {
+             
+            bool getN = n_065(rainVariables.n);
+
+            double A_prev = 0;              //get this! by Linq.
+            double A_next = 0;              //get this! by Linq.
+
+            double z_Aprev = 0;             //get this! by Linq.
+            double z_A_next = 0;            //get this! by Linq.
+
+            double deltaMain = (A_next - A_prev) / (z_A_next - z_Aprev);
+            double diff = rainVariables.A - A_prev;
+
+            double InterpolationResult = z_Aprev + deltaMain * diff;
+
+            return InterpolationResult;
+        }
+
+        //method for z_roadAndRoof
+        bool n_065(double _n)
+        {
+            if (_n < 0.65)
+                return true;
+            else
+                return false;
+        }
     }
 }
