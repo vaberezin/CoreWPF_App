d20427ececf81210baf8a82547a75c4bb0a104be
vladislav <vaberezin@protonmail.com>
2020-07-23 16:51:58 +0300

    Methods z_mid,all 't', beta,mr.gamma,hc written.

--- RainCalcs/RainVariables.cs
+++ RainCalcs/RainVariables.cs
@@ -8,59 +8,59 @@ namespace CoreWpfApp.RainCalcs
     {
         //Variables initialization
 
-        internal double F; //ok+
-        internal double F_road; //ok+
-        internal double F_green;  //ok+
-        internal double F_roof; //ok+
-        internal double F_gravel; //ok+
-        internal double n; //Pril2 table +
-        internal double A;  //method+
-        internal double Psy_mid; //method+
-        internal double Z_mid; //method+
+        internal decimal F; //ok+
+        internal decimal F_road; //ok+
+        internal decimal F_green;  //ok+
+        internal decimal F_roof; //ok+
+        internal decimal F_gravel; //ok+
+        internal decimal n; //Pril2 table +
+        internal decimal A;  //method+
+        internal decimal Psy_mid; //method+
+        internal decimal Z_mid; //method+
         public int N_sections;
-        internal double t_con; //ok+
-        internal double t_can; //method+
-        internal double t_p; //method+
-        internal double l_lotok; //ok+
-        internal double v_lotok; //ok+
-        internal double l_p; //ok+
-        internal double v_p; //ok+
-        internal double Q_rRain; //method+
-        internal double Q_rIce; //method+
-        internal double beta; //table+
-        internal double Q_cal; //method+
+        internal decimal t_con; //ok+
+        internal decimal t_can; //method+
+        internal decimal t_p; //method+
+        internal decimal l_lotok; //ok+
+        internal decimal v_lotok; //ok+
+        internal decimal l_p; //ok+
+        internal decimal v_p; //ok+
+        internal decimal Q_rRain; //method+
+        internal decimal Q_rIce; //method+
+        internal decimal beta; //table+
+        internal decimal Q_cal; //method+
         internal int Q_20; //table+
-        internal double P; //table+
-        internal double m_r; //table
-        internal double gamma; //table
-        internal double K; //table
-        internal double Z_i; //table
-        internal double Psy_i; //table
+        internal decimal P; //table+
+        internal decimal m_r; //table
+        internal decimal gamma; //table
+        internal decimal K; //table
+        internal decimal Z_i; //table
+        internal decimal Psy_i; //table
         internal int h_c; //table
-        public double t_c() //
+        public decimal t_c() //
         {
              return t_p;
         }
-        public double K_y() // -> METHOD 21072020+
+        public decimal K_y() // -> METHOD 21072020+
         
         {
-            double _F_y = F_y();
+            decimal _F_y = F_y();
             return 1 - (_F_y / F);
         }
-        public double F_y() // -> METHOD 21072020+
+        public decimal F_y() // -> METHOD 21072020+
         
         {
             return F_road;
         }
 
-        public double F_roadRoof() //-> METHOD 21072020+
+        public decimal F_roadRoof() //-> METHOD 21072020+
         
         {
             return F_road + F_roof;
             
         }
 
-        public double t_r() //-> METHOD 21072020+
+        public decimal t_r() //-> METHOD 21072020+
         {
              return t_con + t_can + t_p;
         }
--- RainFlowCalcWindow.xaml.cs
+++ RainFlowCalcWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using CoreWpfApp.RainCalcs; //����������111
+using CoreWpfApp.AppDB;
 using MySqlX.XDevAPI.Common;
 
 namespace CoreWpfApp
@@ -42,26 +43,26 @@ namespace CoreWpfApp
         {
             
 
-            rainVariables.F = Double.Parse(F_tot.Text);
-            rainVariables.F_road = Double.Parse(F_road.Text);
-            rainVariables.F_roof = Double.Parse(F_roof.Text);
-            rainVariables.F_gravel = Double.Parse(F_shcheb.Text);
-            rainVariables.F_green = Double.Parse(F_grass.Text);
+            rainVariables.F = decimal.Parse(F_tot.Text);
+            rainVariables.F_road = decimal.Parse(F_road.Text);
+            rainVariables.F_roof = decimal.Parse(F_roof.Text);
+            rainVariables.F_gravel = decimal.Parse(F_shcheb.Text);
+            rainVariables.F_green = decimal.Parse(F_grass.Text);
             rainVariables.Q_20 = Int32.Parse(RainIntensivity.Text);
-            rainVariables.P = Double.Parse(RainPossibility.Text);
-            rainVariables.t_con = Double.Parse(ConcentrationTime.Text);
-            rainVariables.l_lotok = Double.Parse(LotokLength.Text);
-            rainVariables.v_lotok = Double.Parse(LotokVelocity.Text);
-            rainVariables.l_p = Double.Parse(CollectorLength.Text);
-            rainVariables.v_p = Double.Parse(CollectorVelocity.Text);
+            rainVariables.P = decimal.Parse(RainPossibility.Text);
+            rainVariables.t_con = decimal.Parse(ConcentrationTime.Text);
+            rainVariables.l_lotok = decimal.Parse(LotokLength.Text);
+            rainVariables.v_lotok = decimal.Parse(LotokVelocity.Text);
+            rainVariables.l_p = decimal.Parse(CollectorLength.Text);
+            rainVariables.v_p = decimal.Parse(CollectorVelocity.Text);
             rainVariables.N_sections = Int32.Parse(SectionsNumber.Text);
-            rainVariables.n = 
-            getA(rainVariables); //+
-            getPsy_mid(rainVariables);+
-            getZ_mid();
-            gett_r();
-            gett_can();
-            gett_p();
+            rainVariables.n = getN(rainVariables);
+            (decimal)getA(rainVariables); //+
+            getPsy_mid(rainVariables); //+
+            getZ_mid(rainVariables); //+
+            gett_r(rainVariables);
+            //gett_can(rainVariables); // enters to gett_r method
+            //gett_p(rainVariables); // enters to gett_r method
 
             //Target VALUES:->
             rainVariables.Q_rRain();
@@ -70,38 +71,38 @@ namespace CoreWpfApp
             //Target VALUES<-:
 
 
-            rainVariables.beta =;
-            rainVariables.m_r;
-            rainVariables.gamma;
-            rainVariables.K;
-            rainVariables.Z_i;
-            rainVariables.Psy_i;
-            rainVariables.h_c;
+            rainVariables.beta = getBeta(rainVariables);
+            rainVariables.m_r = getMr(rainVariables);
+            rainVariables.gamma = getGamma(rainVariables);
+            //rainVariables.K; //is used if F_tot > 500 ga (not for MVP)
+            //rainVariables.Z_i; //no need at all (method for z mid includes this)
+            //rainVariables.Psy_i; //no need at all (method for z mid includes this)
+            rainVariables.h_c = (int)getHc(rainVariables);
             //rainVariables.t_c; later, not for MVP
             rainVariables.K_y();
             rainVariables.F_y() ;
-            rainVariables.F_roadRoof() ;
+            rainVariables.F_roadRoof();
             
 
 
         }
 
         double getA(RainVariables rainVariables){
-            double _A = 0;
+             double _A = 0;
 
-            _A = rainVariables.Q_20 * Math.Pow(20, rainVariables.n)*Math.Pow((1+(Math.Log10(rainVariables.P)/Math.Log10(rainVariables.m_r))), rainVariables.gamma);
+            _A = rainVariables.Q_20 * Math.Pow(20, (double)rainVariables.n)*Math.Pow((1+(Math.Log10((double)rainVariables.P)/Math.Log10((double)rainVariables.m_r))), (double)rainVariables.gamma);
 
             return _A;
 
         }
 
-        double getPsy_mid(RainVariables rainvariables){
-            double _Psy_mid = 0;
+        decimal getPsy_mid(RainVariables rainvariables){
+            decimal _Psy_mid = 0;
 
-            double K_gravel = rainvariables.F_gravel * 0.4; //tabl10 rek
-            double K_road = rainvariables.F_road * 0.95; //tabl10 rek
-            double K_roof = rainvariables.F_roof * 0.95; //tabl10 rek
-            double K_green = rainvariables.F_green * 0.1; //tabl10 rek
+            decimal K_gravel = rainvariables.F_gravel * 0.4m; //tabl10 rek
+            decimal K_road = rainvariables.F_road * 0.95m; //tabl10 rek
+            decimal K_roof = rainvariables.F_roof * 0.95m; //tabl10 rek
+            decimal K_green = rainvariables.F_green * 0.1m; //tabl10 rek
 
 
             _Psy_mid = (K_gravel + K_green + K_road + K_roof) / rainvariables.F;
@@ -109,46 +110,173 @@ namespace CoreWpfApp
             return _Psy_mid;
         }
         
-        double getZ_mid(RainVariables rainvariables)
+        decimal getZ_mid(RainVariables rainvariables)
         {
-            double _Z_mid = 0;
+            RainVariables rv = rainvariables; //for get_Z_roadAndRoof constructor parameter
+            decimal _Z_mid = 0;
 
-            double K_gravel = rainvariables.F_gravel * 0.125; //tabl10 rek
-            double K_road = rainvariables.F_road * //; //tabl10 rek
-            double K_roof = rainvariables.F_roof * //; //tabl10 rek
-            double K_green = rainvariables.F_green * 0.038; //tabl10 rek
+            decimal K_gravel = rainvariables.F_gravel * 0.125m; //tabl10 rek
+            decimal K_road = rainvariables.F_road * get_Z_roadAndRoof(rv); //tabl10 rek
+            decimal K_roof = rainvariables.F_roof * get_Z_roadAndRoof(rv); //tabl10 rek
+            decimal K_green = rainvariables.F_green * 0.038m; //tabl10 rek
 
-
+            _Z_mid = (K_gravel + K_green + K_road + K_roof)/rainvariables.F;
 
             return _Z_mid;
         }
+            
+        //getting z_road and z_roof values        
 
-        double z_roadAndRoof(RainVariables rainVariables)
+        decimal get_Z_roadAndRoof(RainVariables rainVariables) 
         {
              
-            bool getN = n_065(rainVariables.n);
+            bool getN = n_LESS065(rainVariables.n);
 
-            double A_prev = 0;              //get this! by Linq.
-            double A_next = 0;              //get this! by Linq.
+            int A_prev = (int)Math.Floor(rainVariables.A/100)*100;
+            int A_next = (int)Math.Ceiling(rainVariables.A/100)*100;
 
-            double z_Aprev = 0;             //get this! by Linq.
-            double z_A_next = 0;            //get this! by Linq.
+            decimal z_A_prev = 0;             //get this! by Linq.
+            decimal z_A_next = 0;            //get this! by Linq.
 
-            double deltaMain = (A_next - A_prev) / (z_A_next - z_Aprev);
-            double diff = rainVariables.A - A_prev;
 
-            double InterpolationResult = z_Aprev + deltaMain * diff;
+            //getting z_road and z_roof values from db for further interpolation
+            using(projectappdbContext db = new projectappdbContext()){
+                if(getN==true){
+                    var _z_A_prev = db.Table11ZiVodonepronitsaemie.Where(el => el.A <= A_prev).Select(el => el.NAbove065).FirstOrDefault();
+                    var _z_A_next = db.Table11ZiVodonepronitsaemie.Where(el => el.A >= A_next).Select(el => el.NAbove065).FirstOrDefault();
+                    z_A_prev = _z_A_prev;
+                    z_A_next = _z_A_next;
+                }
+                else{
+                    var _z_A_prev = db.Table11ZiVodonepronitsaemie.Where(el => el.A <= A_prev).Select(el => el.NLess065).FirstOrDefault();
+                    var _z_A_next = db.Table11ZiVodonepronitsaemie.Where(el => el.A >= A_next).Select(el => el.NLess065).FirstOrDefault();
+                    z_A_prev = _z_A_prev;
+                    z_A_next = _z_A_next;
+                }
+                
+            } //end of getting z_road and z_roof values.
 
-            return InterpolationResult;
+            decimal deltaMain = (A_next - A_prev) / (z_A_next - z_A_prev);  //interpolation part
+            decimal diff = rainVariables.A - A_prev;
+
+            decimal InterpolationResult = z_A_prev + deltaMain * diff;
+
+            return InterpolationResult; //end of interpolation part. end of getting z_road and z_roof values
         }
 
         //method for z_roadAndRoof
-        bool n_065(double _n)
+        bool n_LESS065(decimal _n)
         {
-            if (_n < 0.65)
+            if (_n < 0.65m)
                 return true;
             else
                 return false;
+        } //end of method for z_roadAndRoof
+
+        decimal gett_can(RainVariables rainVariables){ //lotki time
+            decimal _t_can = 0;
+            _t_can = 0.021m * rainVariables.l_lotok/rainVariables.v_lotok;
+            return _t_can;
         }
+        decimal gett_p(RainVariables rainVariables){ //collector time
+            decimal _t_p = 0;
+            _t_p = 0.017m * rainVariables.l_p/rainVariables.v_p;
+            return _t_p;
+        }
+
+        decimal gett_r(RainVariables rainVariables){
+            decimal _gett_r;
+            _gett_r = gett_can(rainVariables) + gett_p(rainVariables) + rainVariables.t_con;
+            return _gett_r;
+        }
+
+        decimal getN(RainVariables rainVariables){ //getting n method
+
+            decimal _getN = 0;
+
+            using(projectappdbContext db = new projectappdbContext()){
+                string ChoosenRegion = Region.SelectionBoxItem as string;
+
+                if (rainVariables.P >= 1){
+                    var n = db.Pril2NPlessPaboveMrGamma.Where(el => el.Region == ChoosenRegion && rainVariables.P >= 1).Select(el => el.NPabove1).FirstOrDefault();
+                    _getN = n;
+                }
+                else{
+                    var n = db.Pril2NPlessPaboveMrGamma.Where(el => el.Region == ChoosenRegion && rainVariables.P < 1)
+                                                       .Select(el => el.NPless1)
+                                                       .FirstOrDefault(); 
+                    _getN = n;               
+                }
+                
+            }
+
+            return _getN;
+        } //end of getting n method
+
+        decimal getBeta(RainVariables rainVariables){ //getting beta method
+
+            decimal _beta = 0;
+
+            decimal n = rainVariables.n; 
+
+            if (n <= 0.4m){
+                _beta = 0.8m;
+            }
+            else if(n <= 0.5m){
+                _beta = 0.75m;
+            }
+            else if(n <= 0.7m){
+                _beta = 0.7m;
+            }
+            else{
+                _beta = 0.65m;
+            }
+
+            return _beta;
+
+        }//end of getting beta method
+
+        decimal getMr(RainVariables rainVariables){ //getting mr method
+
+            decimal _getMr = 0;
+
+            using(projectappdbContext db = new projectappdbContext()){
+                string ChoosenRegion = Region.SelectionBoxItem as string;
+
+                var mr = db.Pril2NPlessPaboveMrGamma.Where(el => el.Region == ChoosenRegion).Select(el => el.Mr).FirstOrDefault();
+                _getMr = mr;
+
+            }
+
+            return _getMr;
+        }//end of getting mr method
+
+        decimal getGamma(RainVariables rainVariables){ //getting gamma method
+
+            decimal _getGamma = 0;
+
+            using(projectappdbContext db = new projectappdbContext()){
+                string ChoosenRegion = Region.SelectionBoxItem as string;
+
+                var mr = db.Pril2NPlessPaboveMrGamma.Where(el => el.Region == ChoosenRegion).Select(el => el.Gamma).FirstOrDefault();
+                _getGamma = mr;
+
+            }
+
+            return _getGamma;
+        } //end of getting gamma method
+
+        decimal getHc(RainVariables rainVariables){ //getting gamma method
+
+            decimal _Hc = 0;
+
+            using(projectappdbContext db = new projectappdbContext()){
+                int ClimateIdFromCombobox = 1;
+
+                var Hc = db.Table12SloyTalikhVod.Where(el => el.ClimateId == ClimateIdFromCombobox ).Select(el => el.P015).FirstOrDefault();
+                _Hc = Hc;
+            }
+            return _Hc;
+        } //end of getting gamma method
     }
 }
