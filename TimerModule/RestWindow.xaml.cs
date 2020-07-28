0df488d365d380e744de96dadce27469fc0953df
vladislav <vaberezin@protonmail.com>
2020-07-28 11:28:01 +0300

    Created RestWindow class.

---
+++ TimerModule/RestWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CoreWpfApp;
+using CoreWpfApp.TimerModule;
+
+
+
+namespace CoreWpfApp.TimerModule
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class RestWindow : Window
+    {
+        public RestWindow()
+        {
+            InitializeComponent();
+        }
+        
+        
+        //private void ButtoRainFlowCalcBtn_Click(object sender, RoutedEventArgs e){
+        //   
+        //   RainFlowCalcWindow rainFlowCalc = new RainFlowCalcWindow();
+        //   rainFlowCalc.Show();
+        //}
+    }
+}
